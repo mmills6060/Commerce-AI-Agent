e57bb996698088d4a4e0dc3d813df3a3b028f202
Michael Mills <mmills6060@gmail.com>
2025-11-24 13:50:11 -0500

    another try to fix production docs

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,9 +2,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-import express, { type Request, type Response, type NextFunction } from 'express'
+import express, { type Request, type Response } from 'express'
 import cors from 'cors'
-import swaggerUi from 'swagger-ui-express'
 import { apiRouter } from './routes/api.js'
 import { swaggerSpec } from './config/swagger.js'
 
@@ -22,14 +21,45 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRouter)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-  customCss: '.swagger-ui .topbar { display: none }',
-  customSiteTitle: 'Commerce AI Agent API Documentation',
-  swaggerOptions: {
-    persistAuthorization: true,
-    displayRequestDuration: true
-  }
-}))
+app.get('/api-docs', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Commerce AI Agent API Documentation</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    #swagger-ui {
+      box-sizing: border-box;
+    }
+  </style>
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function () {
+      const ui = SwaggerUIBundle({
+        url: '/api-docs.json',
+        dom_id: '#swagger-ui',
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: 'StandaloneLayout'
+      })
+      window.ui = ui
+    }
+  </script>
+</body>
+</html>`)
+})
 
 app.get('/api-docs.json', (req: Request, res: Response) => {
   res.setHeader('Content-Type', 'application/json')
